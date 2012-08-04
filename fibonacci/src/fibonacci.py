f912c26d53c37c851aaf8abcbb339edc2b811c7a
Dean Thomas <dean.plymouth@googlemail.com>
2012-08-04 21:06:34 +0100

    Large amount of restructuring. Build cache of numbers instead of printing on the fly.

--- fibonacci/src/fibonacci.py
+++ fibonacci/src/fibonacci.py
@@ -4,19 +4,33 @@
 __author__="dean"
 __date__ ="$31-Jul-2012 23:54:11$"
 
+from array import *
+
+fibonacciNumbers = array('l')
+
 def add(a,b):
     return a + b
 
-if __name__ == "__main__":
+def buildCache(maximum):
     c = 1   #   n (current number)
     b = 0   #   n - 1
     a = 0   #   n - 2
     
-    print(0),
-    print(1),
+    #   Manually insert the first two numbers
+    fibonacciNumbers.append(0)
+    fibonacciNumbers.append(1)
     
-    for x in range(100):
+    for x in range(maximum-2):
         a = b
         b = c
         c = add(a,b)
-        print(c),
+        fibonacciNumbers.append(c)
+        #print(c),
+
+def printCache():
+    for x in fibonacciNumbers:
+        print(x),
+
+if __name__ == "__main__":
+    buildCache(50)
+    printCache()
